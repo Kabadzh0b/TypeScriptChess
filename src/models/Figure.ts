51e7ed58bf824ccfc0183e641ca6e27d1dd35f95
Kabadzh0b <chubbudda@gmail.com>
2023-04-01 21:03:41 +0300

    class Figure

--- src/models/Figure.ts
+++ src/models/Figure.ts
@@ -1,3 +1,26 @@
+import { Colors } from "./Colors";
+import { Square } from "./Square";
+import logoType from "../pictures/bb.png";
+
+export enum figureNames{
+    KING = "king",
+    QUEEN = "queen",
+    ROOK = "rook",
+    BISHOP = "bishop",
+    KNIGHT = "knigth",
+    PAWN = "pawn",
+}
+
 export class Figure{
+    color:Colors;
+    square:Square;
+    logo: typeof logoType;
+    name: figureNames;
     
+    constructor(color: Colors, name:figureNames, square: Square, logo:typeof logoType){
+        this.color = color;
+        this.square = square;
+        this.logo = logo;
+        this.name = name;
+    }
 }
